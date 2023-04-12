d77bdf0de2f1446099868eae232c25393e2ce6ff
vinit <vinitkkulkarni@gmail.com>
2023-04-12 18:44:35 +0530

    Added:editing the exist contact

--- AddressBook.java
+++ AddressBook.java
@@ -23,6 +23,18 @@ class Contact {
         this.phoneNumber = phoneNumber;
         this.emailId = emailId;
     }
+
+    public void displayContactDetails(){
+        System.out.println("--- CONTACT DETAILS ---");
+        System.out.println("FIRST NAME => " + firstName);
+        System.out.println("LAST NAME => " + lastName);
+        System.out.println("ADDRESS => " + address);
+        System.out.println("CITY => " + city);
+        System.out.println("STATE => " + state);
+        System.out.println("ZIP => " + zip);
+        System.out.println("PHONE NUMBER => " + phoneNumber);
+        System.out.println("EMAIL ID => " + emailId);
+    }
 }
 
 public class AddressBook {
@@ -57,6 +69,80 @@ public class AddressBook {
         contactArray[0] = new Contact(fName, lName, address, city, state, zip, phoneNumber, email);
     }
 
+    static void editContact(String name, Contact contactArray[]) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean flag = false;
+        if (contactArray[0].firstName.equals(name)) {
+            flag = true;
+            if (flag == true) {
+                System.out.println("1.LastName 2.Address 3.City 4.State 5.ZipCode 6.PhoneNumber 7.Email 8.Exit");
+                int editOption = sc.nextInt();
+                switch (editOption) {
+                    case 1:
+                        System.out.print("Edit last name:");
+                        String tempLastName = sc.next();
+                        contactArray[0].lastName = tempLastName;
+                        break;
+                    case 2:
+                        System.out.print("Edit address:");
+                        String tempAddress = sc.next();
+                        contactArray[0].address = tempAddress;
+                        break;
+                    case 3:
+                        System.out.print("Edit city:");
+                        String tempCity = sc.next();
+                        contactArray[0].city = tempCity;
+                        break;
+                    case 4:
+                        System.out.print("Edit state:");
+                        String tempState = sc.next();
+                        contactArray[0].state = tempState;
+                        break;
+                    case 5:
+                        System.out.print("Edit zip:");
+                        int tempZip = sc.nextInt();
+                        contactArray[0].zip = tempZip;
+                        break;
+                    case 6:
+                        System.out.print("Edit phone:");
+                        long tempPhoneNumber = sc.nextLong();
+                        contactArray[0].phoneNumber = tempPhoneNumber;
+                        break;
+                    case 7:
+                        System.out.print("Edit email:");
+                        String tempEmail = sc.next();
+                        contactArray[0].emailId = tempEmail;
+                        break;
+                    case 8:
+                        System.exit(100);
+                        break;
+                    default:
+                        System.out.println("!!!something went wrong!!!");
+                        break;
+                    }
+                } else {
+                    flag = false;
+                }
+            }
+        if (flag == false)
+            System.out.println(name + " not found in address book");
+    }
+
+    static void displayContacts(Contact contactArray[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("================");
+        System.out.println();
+
+        if(contactArray[0] == null)
+            System.out.println();
+        else
+            contactArray[0].displayContactDetails();
+
+        System.out.println();
+        System.out.println("================");
+    }
+
     public static void main(String[] args) {
         System.out.println(" * * * WELCOME TO ADDRESS BOOK * * * ");
         Contact contactArray[] = new Contact[1];
@@ -64,7 +150,7 @@ public class AddressBook {
 
         int temp = 1;
         while (temp != 0) {
-            System.out.println("[1.ADD_CONTACT]  [2.EXIT]");
+            System.out.println("[1.ADD_CONTACT]  [2.EDIT]  [3.DISPLAY]  [4.EXIT]");
             System.out.print("choice:");
             int choice = sc.nextInt();
 
@@ -73,6 +159,14 @@ public class AddressBook {
                     addContact(contactArray);
                     break;
                 case 2:
+                    System.out.print("Enter person name you want to edit the details:");
+                    String name = sc.next();
+                    editContact(name, contactArray);
+                    break;
+                case 3:
+                    displayContacts(contactArray);
+                    break;
+                case 4:
                     temp = 0;
                     break;
                 default:
