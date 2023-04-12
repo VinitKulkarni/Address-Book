0f4dd1ff6ba625ead7d5dfa8355b0f63873141f9
vinit <vinitkkulkarni@gmail.com>
2023-04-12 18:03:18 +0530

    Added:contact details class

--- AddressBook.java
+++ AddressBook.java
@@ -1,5 +1,15 @@
 package com.bridgelabz.addressbook;
 
+class Contacts {
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    int zip;
+    long phoneNumber;
+    String emailId;
+}
 public class AddressBook {
     public static void main(String[] args) {
         System.out.println(" * * * WELCOME TO ADDRESS BOOK * * * ");
